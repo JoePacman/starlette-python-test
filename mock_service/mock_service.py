c10452ade956cc5da8a56fbcc6b20784c432828f
Joe Packham <joseph.packham@paconsulting.com>
2020-01-26 12:16:36 +0000

    added basic prometheus metrics

--- mock_service/mock_service.py
+++ mock_service/mock_service.py
@@ -3,6 +3,7 @@ import os
 import uuid
 
 import httpx
+from prometheus_client import start_http_server, Summary, Counter
 from starlette.applications import Starlette
 from starlette.background import BackgroundTask
 from starlette.responses import JSONResponse
@@ -14,6 +15,10 @@ client = httpx.AsyncClient()
 CALLBACK_URL = os.environ["CALLBACK_URL"]
 logger = logging.getLogger(__name__)
 
+# prometheus metric 1 - general information about time and count of function calls
+AUTH_TIME = Summary('request_processing_seconds', 'Time spent processing authentication')
+# prometheus metric 2 - failures authenticing
+AUTH_FAILED = Counter('auth_failures', 'Failed authentication attempts')
 
 @app.route("/api/endpoint", methods=["POST"])
 async def fake_endpoint(request):
@@ -46,18 +51,26 @@ async def auth(request):
     except KeyError:
         logger.warning("Auth payload did not contain username and password")
         return JSONResponse(False)
+    return process_auth(usernameJson, passwordJson)
 
-    authHardcoded = {"joe-packham" : "pwd1", "nisarg-mehta" : "pwd2", "fabio-tamagno" : "pwd3"}
+@AUTH_TIME.time()
+def process_auth(usernameJson, passwordJson):
+    authHardcoded = {"joe-packham" : "pwd1"}
     password = authHardcoded.get(usernameJson)
     if password == None:
         logger.warning("Username not found")
+        AUTH_FAILED.inc(1)
         return JSONResponse(False)
 
     if password == passwordJson:
         return JSONResponse(True)
     else:
         logger.warning("Password does not match")
+        AUTH_FAILED.inc(1)
         return JSONResponse(False)
 
 if __name__ == "__main__":
+    # start prometheus logging on 8081
+    start_http_server(8081)
+    # start mock_service on 8080
     uvicorn.run(app, host="0.0.0.0", port=8080)
